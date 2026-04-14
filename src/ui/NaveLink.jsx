2f1c921933d143cc6623fc715a1b9bfa402705a7
Mehedi-tsc <mehedimmc1@gmail.com>
2026-04-14 15:19:30 +0600

    Navlink pathName Change

--- src/ui/NaveLink.jsx
+++ src/ui/NaveLink.jsx
@@ -10,7 +10,7 @@ const NaveLink = () => {
         return (
         <>
         <li className={`${pathName==='/'?'bg-[#244D3F] text-white  font-semibold':'bg-none font-medium text-[#64748B]'}`}> <Link href='/'><Image className={`${pathName==='/'?'brightness-0 invert':''}`} src='/Vector.png' alt='vetorImg' width={15} height={15}/> Home</Link> </li>
-        <li className={`${pathName==='/timeline'?'bg-[#244D3F] text-white  font-semibold':'bg-none font-medium text-[#64748B]'}`}> <Link href='/timeline'><Image className={`${pathName==='/timeline'?'brightness-0 invert':''}`} src='/clock.png' alt='clockrImg' width={15} height={15}/> Timeline</Link> </li>
+        <li className={`${pathName==='/timeline'?'bg-[#244D3F] text-white  font-semibold':'bg-none font-medium text-[#64748B]'}`}> <Link href='/timeline'><Image className={`${pathName==='/timeline'?'brightness-0 invert':''}`} src='/Clock.png' alt='clockrImg' width={15} height={15}/> Timeline</Link> </li>
         <li className={`${pathName==='/stats'?'bg-[#244D3F] text-white  font-semibold':'bg-none font-medium text-[#64748B]'}`}> <Link href='/stats'><Image className={`${pathName==='/stats'?'brightness-0 invert':''}`} src='/ChartLine.png' alt='statsImg' width={15} height={15}/> Stats</Link> </li>
         </>
     );
